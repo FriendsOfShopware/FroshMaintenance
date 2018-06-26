6046e2f219462d9e7f9644324d65336fa68b56fe
Unknown <alexander@goltfisch.de>
2018-06-26 09:41:46 +0200

    First draft of event listeners in the controller
    The view fire the events and the listeners in the controller bind them to methods.

--- Resources/views/backend/frosh_reset/controller/main.js
+++ Resources/views/backend/frosh_reset/controller/main.js
@@ -11,5 +11,35 @@ Ext.define('Shopware.apps.FroshReset.controller.Main', {
         me.mainWindow.show();
 
         me.callParent(arguments);
+
+        me.control({
+            'frosh-reset-main button[action=select-all]': {
+                click: me.onSelectAll
+            },
+            'frosh-reset-main button[action=reset-data]': {
+                click: me.onResetData
+            },
+            'frosh-reset-info button[action=refresh]': {
+                click: me.onRefreshInfo
+            }
+        });
+    },
+
+    onSelectAll: function (btn) {
+        var me = this;
+
+        console.log('select all');
+    },
+
+    onResetData: function (btn) {
+        var me = this;
+
+        console.log('reset data');
+    },
+
+    onRefreshInfo: function (btn) {
+        var me = this;
+
+        console.log('refresh info');
     }
 });

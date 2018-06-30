cfdfb7b9f706ef88e28e3bd19291db49204bfdc9
Unknown <alexander@goltfisch.de>
2018-06-30 08:30:24 +0200

    Update documentation
    Move documentation of return value to the interface.

--- Components/Reset/ResetServiceInterface.php
+++ Components/Reset/ResetServiceInterface.php
@@ -6,36 +6,50 @@ interface ResetServiceInterface
 {
     /**
      * Returns the count of all customers
+     * 
+     * @return array label and count of the customers module
      */
     public function getCustomerCount();
 
     /**
      * Returns the count of all orders
+     * 
+     * @return array label and count of the orders module
      */
     public function getOrderCount();
 
     /**
      * Returns the count of all products
+     * 
+     * @return array label and count of the products module
      */
     public function getProductCount();
 
     /**
      * Returns the count of all number ranges
+     * 
+     * @return array label and count of the number ranges module
      */
     public function getNumberrangeCount();
 
     /**
      * Returns the count of all visitor statistics
+     * 
+     * @return array label and count of the visitor statistics
      */
     public function getStatisticCount();
 
     /**
      * Returns the count of all categories
+     * 
+     * @return array label and count of the category module
      */
     public function getCategoryCount();
 
     /**
      * Returns the count of all emotion worlds
+     * 
+     * @return array label and count of the emotion worlds module
      */
     public function getEmotionWorldCount();
 
